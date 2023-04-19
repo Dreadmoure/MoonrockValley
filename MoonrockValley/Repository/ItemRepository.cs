1c578f4dd16385aa9c3005a76e3d33abdeca6532
denniwind <denniwind@live.dk>
2023-04-19 11:54:46 +0200

    Added : repositories and provider

---
+++ MoonrockValley/Repository/ItemRepository.cs
@@ -0,0 +1,54 @@
+﻿using MoonrockValley.Domain;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MoonrockValley.Repository
+{
+    /// <summary>
+    /// Class for the itemrepository which holds "blueprints" for the specific item
+    /// </summary>
+    public class ItemRepository : Repository
+    {
+        /// <summary>
+        /// Used to add an item to the ItemRepository
+        /// </summary>
+        /// <param name="name">Name of the item</param>
+        /// <param name="ItemType">Item type</param>
+        /// <param name="value">The value of the item</param>
+        public void AddItem(string name, type ItemType, int value)
+        {
+
+        }
+
+        /// <summary>
+        /// Method for returning a list of all the items in the ItemRepository
+        /// </summary>
+        /// <returns>List of Items</returns>
+        public List<Item> GetAllItems()
+        {
+            //return something
+        }
+
+        /// <summary>
+        /// method for updating the item based on its ID
+        /// </summary>
+        /// <param name="Id">The ID of the Item</param>
+        public void UpdateItem(int id)
+        {
+
+        }
+
+        /// <summary>
+        /// method used for deleting an item based on its ID
+        /// </summary>
+        /// <param name="id">The ID of the Item</param>
+        public void DeleteItem(int id)
+        {
+
+        }
+
+    }
+}
---
+++ MoonrockValley/Repository/SQLiteDatabaseProvider.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MoonrockValley.Repository
+{
+    /// <summary>
+    /// class for SQLiteDatabaseProvider which handles the path to the database
+    /// </summary>
+    public class SQLiteDatabaseProvider
+    {
+        private readonly string connectionString;
+
+        /// <summary>
+        /// Constructor for the provider which takes a string
+        /// </summary>
+        /// <param name="connectionString">the path to the database</param>
+        public SQLiteDatabaseProvider(string connectionString)
+        {
+            this.connectionString = connectionString;
+        }
+
+        /// <summary>
+        /// Method for returning a connection based on the string
+        /// </summary>
+        /// <returns>IDbConnection</returns>
+        public IDbConnection CreateConnection()
+        {
+            return new SQLiteConnection(connectionString);
+        }
+    }
+}
