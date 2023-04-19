9f216ddf31e0b5de3dc4ae86c07614ca4ba02502
IdaMarcherJensen <54858@student.eadania.dk>
2023-04-19 12:33:33 +0200

    Domain : Added * added Inventory * added Item * added ItemInstance * added UpgradeInstance

---
+++ MoonrockValley/Domain/Inventory.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MoonrockValley.Domain
+{
+    public class Inventory
+    {
+        /// <summary>
+        /// Primary key, specifying the ID of the inventory, ie the place in the inventory 
+        /// </summary>
+        public int ID { get; set; }
+
+        /// <summary>
+        /// Foreign key, references the id of the specific Item 
+        /// </summary>
+        public int ItemID { get; set; }
+
+        /// <summary>
+        /// The amount of the specific Item, the size of the stack 
+        /// </summary>
+        public int Amount { get; set; }
+    }
+}
--- MoonrockValley/Domain/Item.cs
+++ MoonrockValley/Domain/Item.cs
@@ -6,7 +6,26 @@ using System.Threading.Tasks;
 
 namespace MoonrockValley.Domain
 {
-    internal class Item
+    public class Item
     {
+        /// <summary>
+        /// Primary key, specifying the Item ID 
+        /// </summary>
+        public int ID { get; set; }
+
+        /// <summary>
+        /// Name of the Item 
+        /// </summary>
+        public string Name { get; set; }
+
+        /// <summary>
+        /// The type of Item 
+        /// </summary>
+        public ItemType Type { get; set; }
+
+        /// <summary>
+        /// The value or price of the Item 
+        /// </summary>
+        public int Value { get; set; }
     }
 }
