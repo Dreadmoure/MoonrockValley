ba82386244b92f3bdbf5d8243633a6a7cec21d09
IdaMarcherJensen <54858@student.eadania.dk>
2023-04-19 12:34:22 +0200

    Repository : Added * added InventoryMapper * added ItemInstanceMapper * added ItemMapper * added UpgradeInstanceMapper

---
+++ MoonrockValley/Repository/InventoryMapper.cs
@@ -0,0 +1,37 @@
+﻿using MoonrockValley.Domain;
+using System;
+using System.Collections.Generic;
+using System.Data.SQLite;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MoonrockValley.Repository
+{
+    public class InventoryMapper
+    {
+        /// <summary>
+        /// Read the database table for Inventory
+        /// </summary>
+        /// <param name="reader"></param>
+        /// <returns>A list of the Inventory in the database</returns>
+        public List<Inventory> MapInventoryFromReader(SQLiteDataReader reader)
+        {
+            // make a list for the result 
+            var result = new List<Inventory>();
+
+            // read all data from the database table for Inventory
+            while (reader.Read())
+            {
+                var id = reader.GetInt32(0);
+                var inventoryId = reader.GetInt32(1);
+
+                // add the new Item, with its data, to the result list 
+                result.Add(new Inventory() { ID = id, ItemID = itemId });
+            }
+
+            // return list of all the Items in the game 
+            return result;
+        }
+    }
+}
---
+++ MoonrockValley/Repository/ItemMapper.cs
@@ -0,0 +1,39 @@
+﻿using MoonrockValley.Domain;
+using System;
+using System.Collections.Generic;
+using System.Data.SQLite;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MoonrockValley.Repository
+{
+    public class ItemMapper
+    {
+        /// <summary>
+        /// Read the database table for Item 
+        /// </summary>
+        /// <param name="reader"></param>
+        /// <returns>A list of all Items in the database</returns>
+        public List<Item> MapItemsFromReader(SQLiteDataReader reader)
+        {
+            // make a list for the result 
+            var result = new List<Item>();
+
+            // read all data from the database table for Item 
+            while (reader.Read())
+            {
+                var id = reader.GetInt32(0);
+                var name = reader.GetString(1);
+                var type = reader.GetInt32(2);
+                var value = reader.GetInt32(3); 
+
+                // add the new Item, with its data, to the result list 
+                result.Add(new Item() { ID = id, Name = name, Type = (ItemType)type, Value = value});
+            }
+
+            // return list of all the Items in the game 
+            return result;
+        }
+    }
+}
